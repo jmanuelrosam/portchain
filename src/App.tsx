08f82aacb99f784f30c609c6d6bc4406c0b134f0
José Manuel Rosa <jmanuelrosa@duck.com>
2023-12-08 20:42:25 +0100

    fdeat: schedules can be undefined on loading time

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export function App (): JSX.Element {
   const { data: vessels } = useVessels()
   const schedules = useSchedules(vessels)
 
-  if (vessels === undefined) {
+  if (vessels === undefined || schedules === undefined) {
     return <Loading />
   }
 

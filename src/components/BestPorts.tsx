cbfec6c98202bdadbf6b05eb4485647c1d42885e
José Manuel Rosa <jmanuelrosa@duck.com>
2023-12-08 21:44:39 +0100

    feat: update colors a types for BestPorts component

--- src/components/BestPorts.tsx
+++ src/components/BestPorts.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 
-export function BestPorts ({ ports }): JSX.Element {
+import { getRandomColor } from '@/utils/randomColors'
+import { type PortInfo } from '@/utils/getPortInformation'
+
+export function BestPorts (
+  { ports }:
+  { ports: PortInfo[] }
+): JSX.Element {
+  const randomColor = getRandomColor()
+
   return (
     <ul>
       {ports?.slice(0, 5).map((port) => (
         <li key={port.name} className='flex flex-row items-center gap-3 mb-3 text-xs'>
           <div
             className='grid h-9 w-9 shrink-0 place-content-center rounded-md bg-success-light text-base'
+            style={{ backgroundColor: randomColor[100], color: randomColor[900] }}
           >
             {port.name.charAt(0)}
           </div>

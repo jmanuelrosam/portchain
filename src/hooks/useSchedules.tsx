3cace22e3a5278bb7601b46ab7efbb1bc185c110
José Manuel Rosa <jmanuelrosa@duck.com>
2023-12-08 20:39:08 +0100

    feat: hook to manage and get schedules

---
+++ src/hooks/useSchedules.tsx
@@ -0,0 +1,13 @@
+import useSWR, { type SWRResponse } from 'swr'
+
+import { type Vessel } from '@/api/getVessels'
+import { type Schedule, getSchedules } from '@/api/getSchedules'
+
+export function useSchedules (vessels: Vessel[]): SWRResponse<Schedule[]> | undefined {
+  return useSWR({
+    url: `${import.meta.env.VITE_API_URL as string}/schedule`,
+    vessels
+  },
+  getSchedules
+  )
+}

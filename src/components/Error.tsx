34d5478504f864b2ba71f89febc7f11aacfbb967
José Manuel Rosa <jmanuelrosa@duck.com>
2023-12-08 21:27:48 +0100

    feat: error component to manage error boundaries

---
+++ src/components/Error.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { FallbackProps } from 'react-error-boundary'
+
+export function Error ({ error }: FallbackProps): JSX.Element {
+  return (
+    <div className='bg-white border border-gray-200 text-slate-800 break-words p-5 rounded-md shadow-sm'>
+      <p className='text-red-600'>Ups! Somethign happens!</p>
+      <p><b>Error</b>: {error.message}</p>
+    </div>
+  )
+}

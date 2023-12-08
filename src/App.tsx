c160c7c346222c542428887e98e5cfb91e42a848
José Manuel Rosa <jmanuelrosa@duck.com>
2023-12-08 20:28:04 +0100

    feat: add a basic layotu to show some information

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Card from './components/Card'
 
 export function App (): JSX.Element {
   return (
     <div className='container m-auto my-8'>
-      Portchain
+      <h1 className='text-2xl font-semibold mb-4'>Portchain</h1>
+      <div className='space-y-4'>
+        <div className='grid grid-cols-2 gap-6'>
+          <Card>
+            <Card.Title>Ports with more calls</Card.Title>
+          </Card>
+          <Card>
+            <Card.Title>Ports with less calls</Card.Title>
+          </Card>
+        </div>
+      </div>
     </div>
   )
 }

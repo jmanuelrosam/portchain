36f25b4fe57d7037c41d6f94848209c57b9719ae
José Manuel Rosa <jmanuelrosa@duck.com>
2023-12-08 21:22:21 +0100

    feat: show ports with more and less calls

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
-import Card from '@/components/Card'
-import Loading from '@/components/Loading'
+import { Card } from '@/components/Card'
+import { Loading } from '@/components/Loading'
+import { BestPorts } from '@/components/BestPorts'
 
 import { useVessels } from '@/hooks/useVessels'
 import { useSchedules } from '@/hooks/useSchedules'
+import { getPortInformation } from '@/utils/getPortInformation'
+
+import { type Schedule } from '@/api/getSchedules'
 
 export function App (): JSX.Element {
   const { data: vessels } = useVessels()
@@ -14,6 +18,8 @@ export function App (): JSX.Element {
     return <Loading />
   }
 
+  const ports = getPortInformation(schedules.data as Schedule[])
+
   return (
     <div className='container m-auto my-8'>
       <h1 className='text-2xl font-semibold mb-4'>Portchain</h1>
@@ -21,9 +27,11 @@ export function App (): JSX.Element {
         <div className='grid grid-cols-2 gap-6'>
           <Card>
             <Card.Title>Ports with more calls</Card.Title>
+            <BestPorts ports={ports} />
           </Card>
           <Card>
             <Card.Title>Ports with less calls</Card.Title>
+            <BestPorts ports={ports?.slice(-5)} />
           </Card>
         </div>
       </div>

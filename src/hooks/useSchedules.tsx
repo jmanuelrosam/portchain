d9d64287c5a642d6eea72ef53344821befd2ea5d
José Manuel Rosa <jmanuelrosa@duck.com>
2023-12-08 21:18:33 +0100

    feat: we need to run the same amount of hooks

--- src/hooks/useSchedules.tsx
+++ src/hooks/useSchedules.tsx
@@ -4,14 +4,12 @@ import { type Vessel } from '@/api/getVessels'
 import { type Schedule, getSchedules } from '@/api/getSchedules'
 
 export function useSchedules (vessels?: Vessel[]): SWRResponse<Schedule[]> | undefined {
-  if (vessels === undefined) {
-    return undefined
-  }
-
-  return useSWR({
-    url: `${import.meta.env.VITE_API_URL as string}/schedule`,
-    vessels
-  },
+  return useSWR((vessels != null)
+    ? {
+        url: `${import.meta.env.VITE_API_URL as string}/schedule`,
+        vessels
+      }
+    : null,
   getSchedules
   )
 }

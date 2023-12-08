1f97bba15a78ed83ee024bd5bd29b5bf133c2311
José Manuel Rosa <jmanuelrosa@duck.com>
2023-12-08 20:41:21 +0100

    fix: vessels can be empty

--- src/hooks/useSchedules.tsx
+++ src/hooks/useSchedules.tsx
@@ -3,7 +3,11 @@ import useSWR, { type SWRResponse } from 'swr'
 import { type Vessel } from '@/api/getVessels'
 import { type Schedule, getSchedules } from '@/api/getSchedules'
 
-export function useSchedules (vessels: Vessel[]): SWRResponse<Schedule[]> | undefined {
+export function useSchedules (vessels?: Vessel[]): SWRResponse<Schedule[]> | undefined {
+  if (vessels === undefined) {
+    return undefined
+  }
+
   return useSWR({
     url: `${import.meta.env.VITE_API_URL as string}/schedule`,
     vessels

bf8d70d58d2273b0d4eac82c113c8aea2426e7f0
José Manuel Rosa <jmanuelrosa@duck.com>
2023-12-08 21:01:31 +0100

    feat: get all port information with calls

---
+++ src/utils/getPortInformation.tsx
@@ -0,0 +1,45 @@
+import { type Schedule } from "@/api/getSchedules"
+
+interface PortCall {
+  arrival: string
+  departure: string
+}
+
+export interface PortInfo {
+  name: string
+  calls: PortCall[]
+}
+
+export function getPortInformation (vessels: Schedule[]): PortInfo[] {
+  const callsByPort = vessels
+    .reduce<Record<string, PortCall[]>>(
+    (acc, vessel) => {
+      vessel.portCalls.forEach(portCall => {
+        if (!portCall.isOmitted) {
+          const portInfo = acc[portCall.port.name] ?? []
+          acc[portCall.port.name] = [
+            ...portInfo,
+            {
+              arrival: portCall.arrival,
+              departure: portCall.departure
+            }
+          ]
+        }
+      })
+
+      return acc
+    },
+    {}
+  )
+
+  return Object
+    .entries(callsByPort)
+    .map(([name, calls]) => ({ name, calls }))
+    .toSorted((first, second) => {
+      if (second.calls.length - first.calls.length !== 0) {
+        return second.calls.length - first.calls.length
+      }
+
+      return first.name.localeCompare(second.name)
+    })
+}
